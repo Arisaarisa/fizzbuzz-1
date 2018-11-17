8e66ac444085b3825ffea8740c79d29e334c95a3
arisa <arisankochira10@gmail.com>
2018-11-17 16:25:15 +0900

    FizzBuzz変換を関数化シフトコマンドエンター コマンドD大量

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,20 @@
-number = int(input("1つの自然数をいれてね:"))
+def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(str(number))
 
-if number % 15 == 0:
-    print("FizzBuzz")
-elif number % 3 == 0:
-    print("Fizz")
-elif number % 5 == 0:
-    print("Buzz")
-else:
-    print(str(number))
+
+fizzbuzz_convert(1)
+fizzbuzz_convert(2)
+fizzbuzz_convert(3)
+fizzbuzz_convert(4)
+fizzbuzz_convert(5)
+fizzbuzz_convert(10)
+fizzbuzz_convert(15)
+fizzbuzz_convert(30)
+fizzbuzz_convert(33)
