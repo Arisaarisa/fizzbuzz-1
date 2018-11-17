869b07b987e8364e914dcfa11fc759156e16783a
arisa <arisankochira10@gmail.com>
2018-11-17 14:57:58 +0900

    15の倍数ならFizzBuzzに出力

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,7 @@ number = int(input("1つの自然数をいれてね:"))
 if number % 15 == 0:
     output = "FizzBuzz"
 elif number % 3 == 0:
-    Åoutput = "Fizz"
+    output = "Fizz"
 elif number % 5 == 0:
     output = "Buzz"
 else:
